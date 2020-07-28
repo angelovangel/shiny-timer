e98f484621cb834aa0c62a707423df207334996c
Angel Angelov <aangeloo@gmail.com>
2020-07-28 18:14:19 +0200

    add NoSleep js library

---
+++ js/nosleep.js
@@ -0,0 +1,8 @@
+var noSleep = new NoSleep();
+
+// Enable wake lock.
+// (must be wrapped in a user input event handler e.g. a mouse or touch handler)
+document.addEventListener('click', function enableNoSleep() {
+  document.removeEventListener('click', enableNoSleep, false);
+  noSleep.enable();
+}, false);
